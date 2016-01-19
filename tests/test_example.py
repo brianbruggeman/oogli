4bc8d3cae1aa16112172aa34c16270a75d2a6f88
Brian Bruggeman <Brian.M.Bruggeman@gmail.com>
2016-01-19 09:46:48 -0600

    Fixes test

--- tests/test_example.py
+++ tests/test_example.py
@@ -18,7 +18,7 @@ def test_example():
         #version 410
         out vec4 frag_color;
         void main () {
-            frag_colour = vec4(0.3, 1.0, 0.3, 1.0);
+            frag_color = vec4(0.3, 1.0, 0.3, 1.0);
         }
     '''
 
