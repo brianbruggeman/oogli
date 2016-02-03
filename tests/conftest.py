3db91b690c096264e02c30ced7721949e6c8e604
Brian Bruggeman <bbruggeman@samba.tv>
2016-02-03 15:55:52 -0600

    Expanded tests and parameterized tests

---
+++ tests/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+
+
+@pytest.fixture(scope="module")
+def options():
+    '''Captures configuration data from config file and validates that
+    data is available'''
+
+    green = (0.2, 1.0, 0.2)
+
+    config = {
+        'width': 100,
+        'height': 100,
+        'triangle': [(0.0, 0.5), (-0.5, 0.5), (-0.5, -0.5)],
+        'colors': [green, green, green],
+        'color': green,
+        'checksum': 35587,
+    }
+
+    return config
--- tests/test_example.py
+++ tests/test_example.py
@@ -3,7 +3,8 @@
 import pytest
 
 
-def test_basic_example():
+def test_basic_example(options):
+    '''Tests basic draw api'''
     import oogli
     import numpy as np
     import time
@@ -29,21 +30,14 @@ def test_basic_example():
         error_message = "OpenGL {major}.{minor} is not supported."
         pytest.skip(error_message.format(major=major, minor=minor))
 
-    # Create a program from the shaders
-    #  Note: This will auto request an OpenGL context of 4.1
     program = oogli.Program(v_shader, f_shader)
 
     # Vertices for a 2D Triangle
-    triangle = [(0.0, 0.5), (-0.5, 0.5), (-0.5, -0.5)]
-    # colors = [
-    #     (0.2, 1.0, 0.2),
-    #     (0.2, 1.0, 0.2),
-    #     (0.2, 1.0, 0.2),
-    # ]
+    vertices = options['triangle']
 
     # Setup window and screenshot pixels
-    width, height = 100, 100
-    with oogli.Window(title='Oogli',
+    width, height = options['width'], options['height']
+    with oogli.Window(title='Oogli|Test|Basic Example',
                       width=width, height=height,
                       major=major, minor=minor,
                       focus=False, visible=False) as win:
@@ -55,8 +49,7 @@ def test_basic_example():
         start_time = time.time()
         while running:
             # Render triangle
-            # program.draw(vertices=triangle, colors=colors)
-            program.draw(vertices=triangle)
+            program.draw(vertices=vertices)
             count += 1
             if win.open is False:
                 running = False
@@ -68,12 +61,77 @@ def test_basic_example():
 
     # Checksum image
     pixel_sum = np.sum(pixels)
-    checksum = 35587  # simple green triangle
+    checksum = options['checksum']  # simple green triangle
     print('Draw loop ran in {:>0.2f} sec. {:>.0f} fps'.format(delta, count / delta))
     assert pixel_sum == checksum
 
 
-def test_color_example():
+def test_basic_load_example(options):
+    '''Tests basic load api'''
+    import oogli
+    import numpy as np
+    import time
+
+    v_shader = '''
+        #version 410
+        in vec2 vertices;
+        void main () {
+            gl_Position = vec4(vertices, 0.0, 1.0);
+        }
+    '''
+
+    f_shader = '''
+        #version 410
+        out vec4 frag_color;
+        void main () {
+            frag_color = vec4(0.2, 1.0, 0.2, 1.0);
+        }
+    '''
+
+    major, minor = (4, 1)
+    if not oogli.opengl_supported(major, minor):
+        error_message = "OpenGL {major}.{minor} is not supported."
+        pytest.skip(error_message.format(major=major, minor=minor))
+
+    program = oogli.Program(v_shader, f_shader)
+
+    # Vertices for a 2D Triangle
+    vertices = options['triangle']
+
+    # Setup window and screenshot pixels
+    width, height = options['width'], options['height']
+    with oogli.Window(title='Oogli|Test|Basic Example',
+                      width=width, height=height,
+                      major=major, minor=minor,
+                      focus=False, visible=False) as win:
+        # Main Loop
+        # Loop through only once
+        count = 0
+        count_stop = 1
+        running = True
+        start_time = time.time()
+        program.load(vertices=vertices)
+        while running:
+            # Render triangle
+            program.draw()
+            count += 1
+            if win.open is False:
+                running = False
+            elif count > count_stop:
+                running = False
+            win.cycle()
+            pixels = oogli.screenshot(win)
+        delta = time.time() - start_time
+
+    # Checksum image
+    pixel_sum = np.sum(pixels)
+    checksum = options['checksum']  # simple green triangle
+    print('Draw loop ran in {:>0.2f} sec. {:>.0f} fps'.format(delta, count / delta))
+    assert pixel_sum == checksum
+
+
+def test_color_example(options):
+    '''Tests basic draw api and uses colors'''
     import oogli
     import numpy as np
     import time
@@ -108,15 +166,13 @@ def test_color_example():
     program = oogli.Program(v_shader, f_shader)
 
     # Vertices for a 2D Triangle
-    triangle = [(0.0, 0.5), (-0.5, 0.5), (-0.5, -0.5)]
-    colors = [
-        (0.2, 1.0, 0.2),
-        (0.2, 1.0, 0.2),
-        (0.2, 1.0, 0.2),
-    ]
+    vertices = options['triangle']
 
     # Setup window and screenshot pixels
-    width, height = 100, 100
+    colors = options['colors']
+
+    # Setup window and screenshot pixels
+    width, height = options['width'], options['height']
     with oogli.Window(title='Oogli',
                       width=width, height=height,
                       major=major, minor=minor,
@@ -129,7 +185,7 @@ def test_color_example():
         start_time = time.time()
         while running:
             # Render triangle
-            program.draw(vertices=triangle, colors=colors)
+            program.draw(vertices=vertices, colors=colors)
             count += 1
             if win.open is False:
                 running = False
@@ -141,12 +197,85 @@ def test_color_example():
 
     # Checksum image
     pixel_sum = np.sum(pixels)
-    checksum = 35587  # simple green triangle
+    checksum = options['checksum']  # simple green triangle
     print('Draw loop ran in {:>0.2f} sec. {:>.0f} fps'.format(delta, count / delta))
     assert pixel_sum == checksum
 
 
-def test_uniform_example():
+def test_color_load_example(options):
+    '''Tests basic draw api and uses colors'''
+    import oogli
+    import numpy as np
+    import time
+
+    v_shader = '''
+        #version 410
+        in vec2 vertices;
+        in vec3 colors;
+        out vec3 vcolors;
+        void main () {
+            gl_Position = vec4(vertices, 0.0, 1.0);
+            vcolors = colors;
+        }
+    '''
+
+    f_shader = '''
+        #version 410
+        in vec3 vcolors;
+        out vec4 frag_color;
+        void main () {
+            frag_color = vec4(vcolors, 1.0);
+        }
+    '''
+
+    major, minor = (4, 1)
+    if not oogli.opengl_supported(major, minor):
+        error_message = "OpenGL {major}.{minor} is not supported."
+        pytest.skip(error_message.format(major=major, minor=minor))
+
+    # Create a program from the shaders
+    #  Note: This will auto request an OpenGL context of 4.1
+    program = oogli.Program(v_shader, f_shader)
+
+    # Vertices for a 2D Triangle
+    vertices = options['triangle']
+
+    # Setup window and screenshot pixels
+    colors = options['colors']
+
+    # Setup window and screenshot pixels
+    width, height = options['width'], options['height']
+    with oogli.Window(title='Oogli',
+                      width=width, height=height,
+                      major=major, minor=minor,
+                      focus=False, visible=False) as win:
+        # Main Loop
+        # Loop through only once
+        count = 0
+        count_stop = 1
+        running = True
+        start_time = time.time()
+        program.load(vertices=vertices, colors=colors)
+        while running:
+            # Render triangle
+            program.draw()
+            count += 1
+            if win.open is False:
+                running = False
+            elif count > count_stop:
+                running = False
+            win.cycle()
+            pixels = oogli.screenshot(win)
+        delta = time.time() - start_time
+
+    # Checksum image
+    pixel_sum = np.sum(pixels)
+    checksum = options['checksum']  # simple green triangle
+    print('Draw loop ran in {:>0.2f} sec. {:>.0f} fps'.format(delta, count / delta))
+    assert pixel_sum == checksum
+
+
+def test_uniform_example(options):
     import oogli
     import numpy as np
     import time
@@ -163,10 +292,10 @@ def test_uniform_example():
 
     f_shader = '''
         #version 410
-        uniform vec3 color;
+        uniform vec3 color = vec3(1.0, 0.2, 0.2);
         out vec4 frag_color;
         void main () {
-            frag_color = vec4(colors, 1.0);
+            frag_color = vec4(color, 1.0);
         }
     '''
 
@@ -180,11 +309,11 @@ def test_uniform_example():
     program = oogli.Program(v_shader, f_shader)
 
     # Vertices for a 2D Triangle
-    triangle = [(0.0, 0.5), (-0.5, 0.5), (-0.5, -0.5)]
-    color = (0.2, 1.0, 0.2)  # Green
+    vertices = options['triangle']
+    color = options['color']
 
     # Setup window and screenshot pixels
-    width, height = 100, 100
+    width, height = options['width'], options['height']
     with oogli.Window(title='Oogli',
                       width=width, height=height,
                       major=major, minor=minor,
@@ -196,8 +325,8 @@ def test_uniform_example():
         running = True
         start_time = time.time()
         while running:
-            # Render triangle
-            program.draw(vertices=triangle, color=color)
+            # Render vertices
+            program.draw(vertices=vertices, color=color)
             count += 1
             if win.open is False:
                 running = False
@@ -209,7 +338,76 @@ def test_uniform_example():
 
     # Checksum image
     pixel_sum = np.sum(pixels)
-    checksum = 35587  # simple green triangle
+    checksum = options['checksum']  # simple green triangle
+    print('Draw loop ran in {:>0.2f} sec. {:>.0f} fps'.format(delta, count / delta))
+    assert pixel_sum == checksum
+
+
+def test_uniform_load_example(options):
+    import oogli
+    import numpy as np
+    import time
+
+    v_shader = '''
+        #version 410
+        in vec2 vertices;
+
+        void main () {
+            vec4 pos = vec4(vertices.xy, 0.0, 1.0);
+            gl_Position = pos;
+        }
+    '''
+
+    f_shader = '''
+        #version 410
+        uniform vec3 color = vec3(1.0, 0.2, 0.2);
+        out vec4 frag_color;
+        void main () {
+            frag_color = vec4(color, 1.0);
+        }
+    '''
+
+    major, minor = (4, 1)
+    if not oogli.opengl_supported(major, minor):
+        error_message = "OpenGL {major}.{minor} is not supported."
+        pytest.skip(error_message.format(major=major, minor=minor))
+
+    # Create a program from the shaders
+    #  Note: This will auto request an OpenGL context of 4.1
+    program = oogli.Program(v_shader, f_shader)
+
+    # Vertices for a 2D Triangle
+    vertices = options['triangle']
+    color = options['color']
+
+    # Setup window and screenshot pixels
+    width, height = options['width'], options['height']
+    with oogli.Window(title='Oogli',
+                      width=width, height=height,
+                      major=major, minor=minor,
+                      focus=False, visible=False) as win:
+        # Main Loop
+        # Loop through only once
+        count = 0
+        count_stop = 1
+        running = True
+        start_time = time.time()
+        program.load(vertices=vertices, color=color)
+        while running:
+            # Render vertices
+            program.draw()
+            count += 1
+            if win.open is False:
+                running = False
+            elif count > count_stop:
+                running = False
+            win.cycle()
+            pixels = oogli.screenshot(win)
+        delta = time.time() - start_time
+
+    # Checksum image
+    pixel_sum = np.sum(pixels)
+    checksum = options['checksum']  # simple green triangle
     print('Draw loop ran in {:>0.2f} sec. {:>.0f} fps'.format(delta, count / delta))
     assert pixel_sum == checksum
 
